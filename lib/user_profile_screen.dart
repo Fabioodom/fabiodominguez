83872fe029fa17bd65c229b3c0a36f9df1d0f068
Fabioodom <fabiodominguez2003@gmail.com>
2025-04-07 11:56:52 +0200

    first commit

--- lib/user_profile_screen.dart
+++ lib/user_profile_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
-//Prueba de committtt
+//Prueba de com
 
 class UserProfileScreen extends StatelessWidget {
   const UserProfileScreen({super.key});
